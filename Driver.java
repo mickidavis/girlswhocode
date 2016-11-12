d1a06597789566c49a00c467db72dff7b0e143f8
Nadine <nadineperez96@gmail.com>
2016-11-12 13:43:08 -0500

    add Driver

---
+++ Driver.java
@@ -0,0 +1,9 @@
+public class Driver {
+   public static void main(String[] args) {
+      //create student named Karlie
+      Student karlie = new Student("Karlie Kloss", 113587313, 3.2, 4);
+
+      //have karlie take a test
+      karlie.takeTest();
+   }
+}
